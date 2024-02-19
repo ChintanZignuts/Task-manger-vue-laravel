1d8148200c7e7afc9d37a922fdf219ff39299cc6
Zignuts Technolab <ztlab85@Zignutss-Mac-mini.local>
2024-02-19 16:36:00 +0530

    backend is created and crud for task mangement api route implemented

--- server/app/Http/Controllers/TaskController.php
+++ server/app/Http/Controllers/TaskController.php
@@ -3,8 +3,78 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Models\Task;
 class TaskController extends Controller
 {
-    //
+    //controllr function for display all task
+    public function index()
+    {
+        try{
+            $tasks = Task::all();
+            return response()->json(['tasks' => $tasks]);
+        }catch(\Exception $e){
+            return response()->json(['error'=>'Error fetching tasks'],500);
+        }
+    }
+    // controllr function for show perticuller task
+    public function show($id)
+    {
+        try{
+            $task = Task::findOrFail($id);
+            return response()->json(['task' => $task]);
+        }
+        catch(\Exception $e){
+            return response()->json(['error'=>'Task not found'],404);
+        }
+    }
+    // controllr function for store task
+    public function store(Request $request)
+    {
+        try{
+            $validatedData = $request->validate([
+            'name' => 'required|string',
+            'description' => 'nullable|string',
+            'completed' => 'boolean',
+            'due_date' => 'nullable|date',
+        ]);
+
+        $task = Task::create($validatedData);
+        return response()->json(['task' => $task], 201);
+        }catch (\Exception $e) {
+            return response()->json(['error' => 'Error creating task'], 500);
+        }
+    }
+
+    // controllr function for update task
+    public function update(Request $request, $id)
+    {
+        try{
+            $validatedData = $request->validate([
+            'name' => 'string',
+            'description' => 'nullable|string',
+            'completed' => 'boolean',
+            'due_date' => 'nullable|date',
+            ]);
+
+            $task = Task::findOrFail($id);
+            $task->update($validatedData);
+
+            return response()->json(['task' => $task]);
+        }catch (\Exception $e) {
+            return response()->json(['error' => 'Error update task'], 500);
+        }
+    }
+
+    // controllr function for update task
+    public function destroy($id)
+    {
+    try{
+        $task = Task::findOrFail($id);
+        $task->delete();
+
+        return response()->json(['message' => 'Task deleted successfully']);
+    }catch (\Exception $e) {
+        return response()->json(['error' => 'Error deleting task'], 500);
+    }
+    }
 }
--- server/routes/api.php
+++ server/routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TaskController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,13 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
+Route::prefix('tasks')->group(function () {
+    Route::get('/', [TaskController::class, 'index']);
+    Route::get('/{id}', [TaskController::class, 'show']);
+    Route::post('/', [TaskController::class, 'store']);
+    Route::put('/{id}', [TaskController::class, 'update']);
+    Route::delete('/{id}', [TaskController::class, 'destroy']);
+});
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
