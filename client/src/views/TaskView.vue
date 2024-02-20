69740b50629d086d287d2550ff20ff7aac533738
chintanrupareliya <ckrupareliya2002@gmail.com>
2024-02-20 23:55:56 +0530

    provider for task list update

--- client/src/views/TaskView.vue
+++ client/src/views/TaskView.vue
@@ -1,39 +1,48 @@
 <script setup>
-import { onMounted, ref } from "vue";
+import { onMounted, ref, provide } from "vue";
 import TaskForm from "@/components/Task/TaskForm.vue";
-import axios from 'axios';
+import axios from "axios";
 import TaskList from "../components/Task/Taskbox.vue";
-const tasks= ref([]);
-const show=ref(false);
-const getTask=async()=>{
-  try{
-    const responese=await axios.get("http://127.0.0.1:8000/api/tasks");
-    tasks.value=responese.data.tasks;
-  }catch(error){
-    console.log("error",error);
+const tasks = ref([]);
+const show = ref(false);
+const fetchTasks = async () => {
+  try {
+    const response = await axios.get("http://127.0.0.1:8000/api/tasks");
+    tasks.value = response.data.tasks;
+  } catch (error) {
+    console.error("Failed to fetch tasks:", error);
   }
-}
-const hendleremove=()=>{
-  console.log("remove");
-  show.value=false;
-}
-const createTask={
-  header:"Create Task"
-}
-onMounted(getTask);
+};
+const addNewTask = async (task) => {
+  try {
+    const response = await axios.post("http://127.0.0.1:8000/api/tasks", task);
+    fetchTasks();
+    return response;
+  } catch (error) {
+    console.error("Failed to add new task:", error);
+  }
+};
+provide("addNewTask", addNewTask);
+const hendleremove = () => {
+  show.value = false;
+};
+const createTask = {
+  header: "Create Task",
+};
+onMounted(fetchTasks);
 </script>
 
 <template>
   <div class="">
     <h1>This is task view</h1>
-    <button @click="show=true">Create Task</button>
-      <TaskForm v-bind="createTask" @remove="hendleremove" :show="show"/>
-    <TaskList v-for="task in tasks" :task="task" :key="task.id"/>   
+    <button @click="show = true">Create Task</button>
+    <TaskForm v-bind="createTask" @remove="hendleremove" :show="show" />
+    <TaskList v-for="task in tasks" :task="task" :key="task.id" />
   </div>
 </template>
 
 <style>
-.containor{
+.containor {
   text-align: center;
   min-width: 100vw;
 }
